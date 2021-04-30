228d29495763b43947280be687136e91e418c568
David Ornellas <17051351+Ornellasd@users.noreply.github.com>
2021-04-30 08:51:19 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const User = require('./user');
+
+// mongoose setup
+const mongoDB = 'mongodb://127.0.0.1/blog_api';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.get('/api/users', (req, res) => {
+  User.find({}, (err, users) => {
+    if(err) {
+      return err;
+    } else {
+      console.log(users);
+      // jwt verify here? 
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+
+  User.findOne({ username: username }, (err, user) => {
+    if(err) {
+      console.log('WEEOWEEEOWEE');
+      res.json(err);
+    }
+    if(user && user.password == password) {
+      console.log(`${user.username} logged in successfully!`);
+      const opts = {};
+      opts.expiresIn = 120;
+      const secret = process.env.SECRET_KEY;
+      const token = jwt.sign({ user }, secret, opts);
+      return res.status(200).json({
+        message: 'Auth Passed',
+        token
+      });
+    } else {
+      console.log('Username or password wrong.');
+    }
+  });
+});
+
+app.post('/api/signup', (req, res) => {
+  console.log('SIGN UP TEST');
+});
+
+app.listen(3002, () => console.log('Server started on port 3002'));
